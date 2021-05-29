47e98507e0d91ab0daf6fbef2d577ec83872609f
OjasMalani <79085373+OjasMalani@users.noreply.github.com>
2021-05-29 14:44:48 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+var images= [Swastik.jpg,Pooja.jpg,Ashish.jpg,Pramila.jpg,Ashok.jpg]
+var names= [" Brother-Swastik Malani"," Mother-Pooja Maheshwari"," Father-Ashish Malani"," Grandmother-Pramila Malani","Grandfather-Ashok Malani"]
+
+var i = 0;
+function update()
+{
+    i++;
+    var number_of_family_member_in_array = 5
+    if(i > number_of_family_member_in_array)
+    {
+        i = 0;
+    }
+var updatedImage= images[i];
+var updatedName= names[i];
+document.getElementById("family_member_image").src= updatedImage;
+document.getElementById("family_member_name").src= updatedName;
+}
